3cf7711102ca7a2bfd4e820d123d467cb2a8ea83
Pblorg <pablo.robin@epfl.ch>
2022-11-04 16:03:51 +0100

    5.4 fait QOIEncoder::encodeData

--- src/cs107/ArrayUtils.java
+++ src/cs107/ArrayUtils.java
@@ -2,9 +2,10 @@ package cs107;
 
 /**
  * Utility class to manipulate arrays.
- * @apiNote First Task of the 2022 Mini Project
+ *
  * @author Hamza REMMAL (hamza.remmal@epfl.ch)
  * @version 1.0
+ * @apiNote First Task of the 2022 Mini Project
  * @since 1.0
  */
 public final class ArrayUtils {
@@ -12,7 +13,8 @@ public final class ArrayUtils {
     /**
      * DO NOT CHANGE THIS, MORE ON THAT IN WEEK 7.
      */
-    private ArrayUtils(){}
+    private ArrayUtils() {
+    }
 
     // ==================================================================================
     // =========================== ARRAY EQUALITY METHODS ===============================
@@ -20,27 +22,21 @@ public final class ArrayUtils {
 
     /**
      * Check if the content of both arrays is the same
+     *
      * @param a1 (byte[]) - First array
      * @param a2 (byte[]) - Second array
      * @return (boolean) - true if both arrays have the same content (or both null), false otherwise
      * @throws AssertionError if one of the parameters is null
      */
-    public static boolean equals(byte[] a1, byte[] a2)
-    {
+    public static boolean equals(byte[] a1, byte[] a2) {
         boolean valeursIdentiques = true;
         assert (a1 == null && a2 == null) || (a1 != null && a2 != null);
 
-        if (a1 == null)
-        {
+        if (a1 == null) {
             return valeursIdentiques;
-        }
-
-        else
-        {
-            for (int i = 0; i < a1.length; i++)
-            {
-                if (a1[i] != a2[i])
-                {
+        } else {
+            for (int i = 0; i < a1.length; i++) {
+                if (a1[i] != a2[i]) {
                     valeursIdentiques = false;
                     break;
                 }
@@ -52,29 +48,25 @@ public final class ArrayUtils {
 
     /**
      * Check if the content of both arrays is the same
+     *
      * @param a1 (byte[][]) - First array
      * @param a2 (byte[][]) - Second array
      * @return (boolean) - true if both arrays have the same content (or both null), false otherwise
      * @throws AssertionError if one of the parameters is null
      */
-    public static boolean equals(byte[][] a1, byte[][] a2)
-    {
+    public static boolean equals(byte[][] a1, byte[][] a2) {
         boolean valeursIdentiques = true;
         assert (a1 == null && a2 == null) || (a1 != null && a2 != null);
 
-        if (a1 == null)
-        {
+        if (a1 == null) {
             return valeursIdentiques;
         }
 
         assert a1.length == a2.length;
 
-        for (int i = 0; i < a1.length; i++)
-        {
-            for (int j = 0; j < a1[i].length; j++)
-            {
-                if (a1[i][j] != a2[i][j])
-                {
+        for (int i = 0; i < a1.length; i++) {
+            for (int j = 0; j < a1[i].length; j++) {
+                if (a1[i][j] != a2[i][j]) {
                     valeursIdentiques = false;
                     break;
                 }
@@ -94,8 +86,7 @@ public final class ArrayUtils {
      * @param value (byte) - value to wrap
      * @return (byte[]) - array with one element (value)
      */
-    public static byte[] wrap(byte value)
-    {
+    public static byte[] wrap(byte value) {
         byte[] wrap = new byte[1];
         wrap[0] = value;
         return wrap;
@@ -114,8 +105,7 @@ public final class ArrayUtils {
      * @return (int) - Integer representation of the array
      * @throws AssertionError if the input is null or the input's length is different from 4
      */
-    public static int toInt(byte[] bytes)
-    {
+    public static int toInt(byte[] bytes) {
         assert bytes != null && bytes.length == 4;
 
         int val1 = (bytes[0] & 0xFF) << 24;
@@ -129,17 +119,17 @@ public final class ArrayUtils {
     /**
      * Separate the Integer (word) to 4 bytes. The Memory layout of this integer is "Big Endian"
      * (See handout for the definition of "Big Endian")
+     *
      * @param value (int) - The integer
      * @return (byte[]) - Big Endian representation of the integer
      */
-    public static byte[] fromInt(int value)
-    {
-        byte a = (byte)(value >> 24);
-        byte b = (byte)(value >> 16 & 0xFF);
-        byte c = (byte)(value >> 8 & 0xFF);
-        byte d = (byte)(value & 0xFF);
+    public static byte[] fromInt(int value) {
+        byte a = (byte) (value >> 24);
+        byte b = (byte) (value >> 16 & 0xFF);
+        byte c = (byte) (value >> 8 & 0xFF);
+        byte d = (byte) (value & 0xFF);
 
-        return new byte[]{a,b,c,d};
+        return new byte[]{a, b, c, d};
     }
 
     // ==================================================================================
@@ -148,30 +138,26 @@ public final class ArrayUtils {
 
     /**
      * Concatenate a given sequence of bytes and stores them in an array
+     *
      * @param tabs (byte ...) - Sequence of bytes to store in the array
      * @return (byte[]) - Array representation of the sequence
      * @throws AssertionError if the input is null
      */
-    public static byte[] concat(byte[] ... tabs)
-    {
+    public static byte[] concat(byte[] ... tabs) {
         assert tabs != null;
         int nombreElement = 0;
         int indice = 0;
 
-        for (int i = 0; i < tabs.length; i++)
-        {
-            for (int j = 0; j < tabs[i].length; j++)
-            {
+        for (int i = 0; i < tabs.length; i++) {
+            for (int j = 0; j < tabs[i].length; j++) {
                 nombreElement++;
             }
         }
 
         byte[] tableauFinal = new byte[nombreElement];
 
-        for (int i = 0; i < tabs.length; i++)
-        {
-            for (int j = 0; j < tabs[i].length; j++)
-            {
+        for (int i = 0; i < tabs.length; i++) {
+            for (int j = 0; j < tabs[i].length; j++) {
                 tableauFinal[indice] = tabs[i][j];
                 indice++;
             }
@@ -181,13 +167,13 @@ public final class ArrayUtils {
 
     /**
      * Concatenate a given sequence of arrays into one array
+     *
      * @param bytes (byte[] ...) - Sequence of arrays
      * @return (byte[]) - Array representation of the sequence
      * @throws AssertionError if the input is null
-     * or one of the inner arrays of input is null.
+     *                        or one of the inner arrays of input is null.
      */
-    public static byte[] concat(byte ... bytes)
-    {
+    public static byte[] concat(byte... bytes) {
         assert bytes != null;
 
         byte[] tab = new byte[bytes.length];
@@ -206,19 +192,19 @@ public final class ArrayUtils {
 
     /**
      * Extract an array from another array
-     * @param input (byte[]) - Array to extract from
-     * @param start (int) - Index in the input array to start the extract from
+     *
+     * @param input  (byte[]) - Array to extract from
+     * @param start  (int) - Index in the input array to start the extract from
      * @param length (int) - The number of bytes to extract
      * @return (byte[]) - The extracted array
      * @throws AssertionError if the input is null or start and length are invalid.
-     * start + length should also be smaller than the input's length
+     *                        start + length should also be smaller than the input's length
      */
-    public static byte[] extract(byte[] input, int start, int length)
-    {
+    public static byte[] extract(byte[] input, int start, int length) {
         assert input != null;
         assert start > 0;
         assert length >= 0;
-        assert (start+length) <= input.length;
+        assert (start + length) <= input.length;
 
         byte[] extracted = new byte[length];
 
@@ -234,21 +220,20 @@ public final class ArrayUtils {
     /**
      * Create a partition of the input array.
      * (See handout for more information on how this method works)
+     *
      * @param input (byte[]) - The original array
      * @param sizes (int ...) - Sizes of the partitions
      * @return (byte[][]) - Array of input's partitions.
      * The order of the partition is the same as the order in sizes
      * @throws AssertionError if one of the parameters is null
-     * or the sum of the elements in sizes is different from the input's length
+     *                        or the sum of the elements in sizes is different from the input's length
      */
-    public static byte[][] partition(byte[] input, int ... sizes)
-    {
+    public static byte[][] partition(byte[] input, int... sizes) {
         assert input != null && sizes != null;
 
         int somme = 0;
 
-        for (int i = 0; i < sizes.length; i++)
-        {
+        for (int i = 0; i < sizes.length; i++) {
             somme += sizes[i];
         }
 
@@ -258,12 +243,10 @@ public final class ArrayUtils {
 
         int indice = 0;
 
-        for (int i = 0; i < sizes.length; i++)
-        {
+        for (int i = 0; i < sizes.length; i++) {
             partition[i] = new byte[sizes[i]];
 
-            for(int j = 0; j < sizes[i]; j++)
-            {
+            for (int j = 0; j < sizes[i]; j++) {
                 partition[i][j] = input[indice];
                 indice++;
             }
@@ -282,17 +265,16 @@ public final class ArrayUtils {
      * a 2-dim byte array where the first dimension is the pixel
      * and the second dimension is the channel.
      * See handouts for more information on the format.
+     *
      * @param input (int[][]) - image data
      * @return (byte [][]) - formatted image data
      * @throws AssertionError if the input is null
-     * or one of the inner arrays of input is null
+     *                        or one of the inner arrays of input is null
      */
-    public static byte[][] imageToChannels(int[][] input)
-    {
+    public static byte[][] imageToChannels(int[][] input) {
         assert input != null;
 
-        for (int i = 0; i < input.length - 1; i++)
-        {
+        for (int i = 0; i < input.length - 1; i++) {
             assert input[i] != null;
             assert input[i].length == input[i + 1].length;
         }
@@ -303,16 +285,14 @@ public final class ArrayUtils {
         byte[][] output = new byte[lignes * colonnes][4];
         int indice = 0;
 
-        for (int i = 0; i < lignes; i++)
-        {
-            for (int j = 0; j < colonnes; j++)
-            {
+        for (int i = 0; i < lignes; i++) {
+            for (int j = 0; j < colonnes; j++) {
                 byte[] bytes = ArrayUtils.fromInt(input[i][j]);
                 byte temp = bytes[0];
-                bytes [0] = bytes[1];
-                bytes [1] = bytes[2];
-                bytes [2] = bytes[3];
-                bytes [3] = temp;
+                bytes[0] = bytes[1];
+                bytes[1] = bytes[2];
+                bytes[2] = bytes[3];
+                bytes[3] = temp;
 
                 output[indice] = bytes;
                 indice++;
@@ -327,43 +307,39 @@ public final class ArrayUtils {
      * Format a 2-dim byte array where the first dimension is the pixel
      * and the second is the channel to a 2-dim int array where the first
      * dimension is the height and the second is the width
-     * @param input (byte[][]) : linear representation of the image
+     *
+     * @param input  (byte[][]) : linear representation of the image
      * @param height (int) - Height of the resulting image
-     * @param width (int) - Width of the resulting image
+     * @param width  (int) - Width of the resulting image
      * @return (int[][]) - the image data
      * @throws AssertionError if the input is null
-     * or one of the inner arrays of input is null
-     * or input's length differs from width * height
-     * or height is invalid
-     * or width is invalid
+     *                        or one of the inner arrays of input is null
+     *                        or input's length differs from width * height
+     *                        or height is invalid
+     *                        or width is invalid
      */
-    public static int[][] channelsToImage(byte[][] input, int height, int width)
-    {
+    public static int[][] channelsToImage(byte[][] input, int height, int width) {
         assert ((height >= 0) && (width >= 0));
         assert input != null;
         assert input.length == height * width;
 
-        for (int i = 0; i < input.length; i++)
-        {
+        for (int i = 0; i < input.length; i++) {
             assert input[i] != null;
         }
 
-        for(int i = 0; i < input.length; i++)
-        {
+        for (int i = 0; i < input.length; i++) {
             byte temp = input[i][3];
-            input [i][3] = input[i][2];
-            input [i][2] = input[i][1];
-            input [i][1] = input[i][0];
-            input [i][0] = temp;
+            input[i][3] = input[i][2];
+            input[i][2] = input[i][1];
+            input[i][1] = input[i][0];
+            input[i][0] = temp;
         }
 
         int[][] output = new int[height][width];
         int indice = 0;
 
-        for (int i = 0; i < height; i++)
-        {
-            for(int j = 0; j < width; j++)
-            {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
                 output[i][j] = ArrayUtils.toInt(input[indice]);
                 indice++;
             }
--- src/cs107/QOIEncoder.java
+++ src/cs107/QOIEncoder.java
@@ -1,12 +1,16 @@
 package cs107;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static cs107.QOISpecification.*;
 
 /**
  * "Quite Ok Image" Encoder
- * @apiNote Second task of the 2022 Mini Project
+ *
  * @author Hamza REMMAL (hamza.remmal@epfl.ch)
  * @version 1.0
+ * @apiNote Second task of the 2022 Mini Project
  * @since 1.0
  */
 public final class QOIEncoder {
@@ -14,7 +18,8 @@ public final class QOIEncoder {
     /**
      * DO NOT CHANGE THIS, MORE ON THAT IN WEEK 7.
      */
-    private QOIEncoder(){}
+    private QOIEncoder() {
+    }
 
     // ==================================================================================
     // ============================ QUITE OK IMAGE HEADER ===============================
@@ -22,13 +27,13 @@ public final class QOIEncoder {
 
     /**
      * Generate a "Quite Ok Image" header using the following parameters
+     *
      * @param image (Helper.Image) - Image to use
-     * @throws AssertionError if the colorspace or the number of channels is corrupted or if the image is null.
-     *  (See the "Quite Ok Image" Specification or the handouts of the project for more information)
      * @return (byte[]) - Corresponding "Quite Ok Image" Header
+     * @throws AssertionError if the colorspace or the number of channels is corrupted or if the image is null.
+     *                        (See the "Quite Ok Image" Specification or the handouts of the project for more information)
      */
-    public static byte[] qoiHeader(Helper.Image image)
-    {
+    public static byte[] qoiHeader(Helper.Image image) {
         assert image != null;
         assert image.channels() == QOISpecification.RGB || image.channels() == QOISpecification.RGBA;
         assert image.color_space() == QOISpecification.sRGB || image.color_space() == QOISpecification.ALL;
@@ -48,12 +53,12 @@ public final class QOIEncoder {
 
     /**
      * Encode the given pixel using the QOI_OP_RGB schema
+     *
      * @param pixel (byte[]) - The Pixel to encode
-     * @throws AssertionError if the pixel's length is not 4
      * @return (byte[]) - Encoding of the pixel using the QOI_OP_RGB schema
+     * @throws AssertionError if the pixel's length is not 4
      */
-    public static byte[] qoiOpRGB(byte[] pixel)
-    {
+    public static byte[] qoiOpRGB(byte[] pixel) {
         assert pixel.length == 4;
 
         byte b0 = QOISpecification.QOI_OP_RGB_TAG;
@@ -67,12 +72,12 @@ public final class QOIEncoder {
 
     /**
      * Encode the given pixel using the QOI_OP_RGBA schema
+     *
      * @param pixel (byte[]) - The pixel to encode
-     * @throws AssertionError if the pixel's length is not 4
      * @return (byte[]) Encoding of the pixel using the QOI_OP_RGBA schema
+     * @throws AssertionError if the pixel's length is not 4
      */
-    public static byte[] qoiOpRGBA(byte[] pixel)
-    {
+    public static byte[] qoiOpRGBA(byte[] pixel) {
         assert pixel.length == 4;
 
         byte b0 = QOISpecification.QOI_OP_RGBA_TAG;
@@ -86,66 +91,63 @@ public final class QOIEncoder {
 
     /**
      * Encode the index using the QOI_OP_INDEX schema
+     *
      * @param index (byte) - Index of the pixel
-     * @throws AssertionError if the index is outside the range of all possible indices
      * @return (byte[]) - Encoding of the index using the QOI_OP_INDEX schema
+     * @throws AssertionError if the index is outside the range of all possible indices
      */
-    public static byte[] qoiOpIndex(byte index)
-    {
+    public static byte[] qoiOpIndex(byte index) {
         assert index > 0 && index < 64;
 
-        return ArrayUtils.wrap((byte)(QOI_OP_INDEX_TAG | index));
+        return ArrayUtils.wrap((byte) (QOI_OP_INDEX_TAG | index));
     }
 
     /**
      * Encode the difference between 2 pixels using the QOI_OP_DIFF schema
+     *
      * @param diff (byte[]) - The difference between 2 pixels
-     * @throws AssertionError if diff doesn't respect the constraints or diff's length is not 3
-     * (See the handout for the constraints)
      * @return (byte[]) - Encoding of the given difference
+     * @throws AssertionError if diff doesn't respect the constraints or diff's length is not 3
+     *                        (See the handout for the constraints)
      */
-    public static byte[] qoiOpDiff(byte[] diff)
-    {
+    public static byte[] qoiOpDiff(byte[] diff) {
         assert diff != null;
         assert diff.length == 3;
 
-        for (int i = 0; i < diff.length; i++)
-        {
+        for (int i = 0; i < diff.length; i++) {
             assert diff[i] > -3 && diff[i] < 2;
             diff[i] = (byte) (diff[i] + 2);
         }
 
-
-        byte b0 = (byte)(diff[0] << 4);
-        byte b1 = (byte)(diff[1] << 2);
+        byte b0 = (byte) (diff[0] << 4);
+        byte b1 = (byte) (diff[1] << 2);
         byte b2 = diff[2];
 
-        return ArrayUtils.wrap((byte)(QOI_OP_DIFF_TAG | b0 | b1 | b2));
+        return ArrayUtils.wrap((byte) (QOI_OP_DIFF_TAG | b0 | b1 | b2));
     }
 
     /**
      * Encode the difference between 2 pixels using the QOI_OP_LUMA schema
+     *
      * @param diff (byte[]) - The difference between 2 pixels
-     * @throws AssertionError if diff doesn't respect the constraints
-     * or diff's length is not 3
-     * (See the handout for the constraints)
      * @return (byte[]) - Encoding of the given difference
+     * @throws AssertionError if diff doesn't respect the constraints
+     *                        or diff's length is not 3
+     *                        (See the handout for the constraints)
      */
-    public static byte[] qoiOpLuma(byte[] diff)
-    {
+    public static byte[] qoiOpLuma(byte[] diff) {
         assert diff != null;
         assert diff.length == 3;
         assert diff[1] > -33 && diff[1] < 32;
         assert diff[0] - diff[1] > -9 && diff[0] - diff[1] < 8;
         assert diff[2] - diff[1] > -9 && diff[2] - diff[1] < 8;
 
-        byte b0 = (byte)(diff[1] + 32);
-        byte b1 = (byte)((diff[0] - diff[1] + 8) << 4);
-        byte b2 = (byte)(diff[2] - diff[1] + 8);
+        byte b0 = (byte) (diff[1] + 32);
+        byte b1 = (byte) ((diff[0] - diff[1] + 8) << 4);
+        byte b2 = (byte) (diff[2] - diff[1] + 8);
 
-        byte[] encoding1 = ArrayUtils.wrap((byte)(QOI_OP_LUMA_TAG | b0));
-        byte[] encoding2 = ArrayUtils.wrap((byte)(b1 | b2));
-
+        byte[] encoding1 = ArrayUtils.wrap((byte) (QOI_OP_LUMA_TAG | b0));
+        byte[] encoding2 = ArrayUtils.wrap((byte) (b1 | b2));
 
 
         return ArrayUtils.concat(encoding1, encoding2);
@@ -153,15 +155,15 @@ public final class QOIEncoder {
 
     /**
      * Encode the number of similar pixels using the QOI_OP_RUN schema
+     *
      * @param count (byte) - Number of similar pixels
-     * @throws AssertionError if count is not between 0 (exclusive) and 63 (exclusive)
      * @return (byte[]) - Encoding of count
+     * @throws AssertionError if count is not between 0 (exclusive) and 63 (exclusive)
      */
-    public static byte[] qoiOpRun(byte count)
-    {
+    public static byte[] qoiOpRun(byte count) {
         assert count > 0 && count < 63;
 
-        return ArrayUtils.wrap((byte)(QOI_OP_RUN_TAG | (count - 1)));
+        return ArrayUtils.wrap((byte) (QOI_OP_RUN_TAG | (count - 1)));
     }
 
     // ==================================================================================
@@ -171,11 +173,11 @@ public final class QOIEncoder {
     /**
      * Encode the given image using the "Quite Ok Image" Protocol
      * (See handout for more information about the "Quite Ok Image" protocol)
+     *
      * @param image (byte[][]) - Formatted image to encode
      * @return (byte[]) - "Quite Ok Image" representation of the image
      */
-    public static byte[] encodeData(byte[][] image)
-    {
+    public static byte[] encodeData(byte[][] image) {
         assert image != null;
 
         byte[] pixelPrecedent = QOISpecification.START_PIXEL;
@@ -183,56 +185,99 @@ public final class QOIEncoder {
         int compteur = 0;
         List<byte[]> arrayList = new ArrayList<>();
 
-        for (int i = 0; i < image.length; i++)
-        {
+        for (int i = 0; i < image.length; i++) {
             assert image[i] != null;
             assert image[i].length == 4;
         }
 
-        for (int i = 0; i < image.length; i++)
-        {
+        for (int i = 0; i < image.length; i++) {
             byte[] pixel = image[i];
             if (ArrayUtils.equals(pixel, pixelPrecedent))
             {
                 compteur++;
-
-                if (compteur == 62 || i == image.length - 1)
-                {
-                   arrayList.add(qoiOpRun((byte) compteur));
-                   compteur = 0;
-                }
-            }
-
-            else
-            {
-                if (compteur != 0)
-                {
+                if (compteur == 62 || i == image.length - 1) {
                     arrayList.add(qoiOpRun((byte) compteur));
+                    compteur = 0;
+                }
+                continue;
+            } else {
+                if (compteur != 0) {
+                    arrayList.add(qoiOpRun((byte) compteur));
+                    compteur = 0;
                 }
             }
 
-            if (ArrayUtils.equals(tableDeHachage[hash(pixel)], pixel))
-            {
-                arrayList.add(qoiOpIndex((byte) i));
-            }
+            if (tryHashEncode(tableDeHachage, arrayList, (byte) i, pixel)) continue;
 
-            else
-            {
-                tableDeHachage[hash(pixel)] = pixel;    
+            if (pixel[3] == pixelPrecedent[3]) {
+                if (tryDiffEncode(pixelPrecedent, arrayList, pixel)) continue;
+
+                if (tryLumaEncode(arrayList, pixel, pixelPrecedent)) continue;
+                arrayList.add(QOIEncoder.qoiOpRGB(pixel));
+            } else {
+                arrayList.add(qoiOpRGBA(pixel));
             }
         }
 
+        byte[][] newArray = new byte[arrayList.size()][];
+        for(int i = 0; i < arrayList.size(); ++i){
+           newArray[i] = arrayList.get(i);
+
+        }
+        return ArrayUtils.concat(newArray);
     }
+
+    private static boolean tryLumaEncode(List<byte[]> arrayList, byte[] pixel, byte[] pixelPrecedent) {
+        byte dr = (byte)(pixel[0] - pixelPrecedent[0]);
+        byte dg = (byte)(pixel[1] - pixelPrecedent[1]);
+        byte db = (byte)(pixel[2] - pixelPrecedent[2]);
+
+        if ((dg > -33 && dg < 32)
+                && (dr - dg > -9 && dr - dg < 8)
+                && (db - dg > -9 && db - dg < 8)) {
+            arrayList.add(QOIEncoder.qoiOpLuma(ArrayUtils.concat(dr, dg, db)));
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean tryDiffEncode(byte[] pixelPrecedent, List<byte[]> arrayList, byte[] pixel) {
+        int compteurDiff = 0;
+        for (int j = 0; j < 3; j++) {
+            if ((pixel[j] - pixelPrecedent[j]) > -3 && (pixel[j] - pixelPrecedent[j]) < 2) {
+                ++compteurDiff;
+            }
+        }
+        if (compteurDiff == 3) {
+            byte[] delta = {(byte)(pixel[0] - pixelPrecedent[0]), (byte)(pixel[1] - pixelPrecedent[1]), (byte)(pixel[2] - pixelPrecedent[2])};
+
+            arrayList.add(QOIEncoder.qoiOpDiff(delta));
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean tryHashEncode(byte[][] tableDeHachage, List<byte[]> arrayList, byte i, byte[] pixel) {
+        if (ArrayUtils.equals(tableDeHachage[hash(pixel)], pixel)) {
+            arrayList.add(qoiOpIndex(i));
+            return true;
+        } else {
+            tableDeHachage[hash(pixel)] = pixel;
+        }
+        return false;
+    }
+
 
     /**
      * Creates the representation in memory of the "Quite Ok Image" file.
-     * @apiNote THE FILE IS NOT CREATED YET, THIS IS JUST ITS REPRESENTATION.
-     * TO CREATE THE FILE, YOU'LL NEED TO CALL Helper::write
+     *
      * @param image (Helper.Image) - Image to encode
      * @return (byte[]) - Binary representation of the "Quite Ok File" of the image
      * @throws AssertionError if the image is null
+     * @apiNote THE FILE IS NOT CREATED YET, THIS IS JUST ITS REPRESENTATION.
+     * TO CREATE THE FILE, YOU'LL NEED TO CALL Helper::write
      */
-    public static byte[] qoiFile(Helper.Image image){
+    public static byte[] qoiFile(Helper.Image image) {
         return Helper.fail("Not Implemented");
     }
 
